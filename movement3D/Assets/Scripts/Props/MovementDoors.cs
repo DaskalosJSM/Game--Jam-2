4945f22348c909454e1f951777138411e691133c
Natalia Gonzalez <73859972+Nataliasita@users.noreply.github.com>
2022-11-11 20:40:36 -0500

    integrations art scene tutorial

---
+++ movement3D/Assets/Scripts/Props/MovementDoors.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementDoors : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider collider) {
+
+        if(collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
